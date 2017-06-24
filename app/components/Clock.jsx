0ba1a1b5c9930b22743e2b9de19d828a6abb9521
Shayne Meyer <shayne.meyer@gmail.com>
2017-06-24 06:07:10 -0700

    create clock component

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,6 +1,9 @@
 var React = require('react');
 
 var Clock = React.createClass({
+  getDefaultProps: function () {
+    totalSeconds: 0
+  },
   formatSeconds: function (totalSeconds) {
     var seconds = totalSeconds % 60;
     var minutes = Math.floor(totalSeconds / 60);
@@ -16,9 +19,13 @@ var Clock = React.createClass({
     return `${minutes}:${seconds}`;
   },
   render: function () {
-    return (
-      <div>
+    var { totalSeconds } = this.props;
 
+    return (
+      <div className="clock">
+        <span className="clock-text">
+          {this.formatSeconds(totalSeconds)}
+        </span>
       </div>
     )
   }
